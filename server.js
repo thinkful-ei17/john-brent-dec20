28395f5c6d3a71e870d11082ab3a20788489018e
JohnLCastillo <30304280+JohnLCastillo@users.noreply.github.com>
2017-12-20 15:04:58 -0500

    Add files via upload
    Solved PUT

--- server.js
+++ server.js
@@ -1,14 +1,10 @@
 
 const express = require('express');
-// we'll use morgan to log the HTTP layer
+const router = express.Router();
 const morgan = require('morgan');
-// we'll use body-parser's json() method to 
-// parse JSON data sent in requests to this app
 const bodyParser = require('body-parser');
 
-// we import the ShoppingList model, which we'll
-// interact with in our GET endpoint
-const {ShoppingList} = require('./models');
+const {ShoppingList, Recipes} = require('./models');
 
 const jsonParser = bodyParser.json();
 const app = express();
@@ -17,18 +13,127 @@ const app = express();
 app.use(morgan('common'));
 
 // we're going to add some items to ShoppingList
-// so there's some data to look at. Note that 
-// normally you wouldn't do this. Usually your
-// server will simply expose the state of the
-// underlying database.
+// so there's some data to look at
 ShoppingList.create('beans', 2);
 ShoppingList.create('tomatoes', 3);
 ShoppingList.create('peppers', 4);
 
-// when the root of this route is called with GET, return
-// all current ShoppingList items by calling `ShoppingList.get()`
+// adding some recipes to `Recipes` so there's something
+// to retrieve.
+Recipes.create(
+  'boiled white rice', ['1 cup white rice', '2 cups water', 'pinch of salt']);
+Recipes.create(
+  'milkshake', ['2 tbsp cocoa', '2 cups vanilla ice cream', '1 cup milk']);
+
+// when the root of this router is called with GET, return
+// all current ShoppingList items
 app.get('/shopping-list', (req, res) => {
   res.json(ShoppingList.get());
+});
+
+app.post('/shopping-list', jsonParser, (req, res) => {
+  // ensure `name` and `budget` are in request body
+  const requiredFields = ['name', 'budget'];
+  for (let i=0; i<requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+
+  const item = ShoppingList.create(req.body.name, req.body.budget);
+  res.status(201).json(item);
+});
+
+// when PUT request comes in with updated item, ensure has
+// required fields. also ensure that item id in url path, and
+// item id in updated item object match. if problems with any
+// of that, log error and send back status code 400. otherwise
+// call `ShoppingList.update` with updated item.
+app.put('/shopping-list/:id', jsonParser, (req, res) => {
+  const requiredFields = ['name', 'budget', 'id'];
+  for (let i=0; i<requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+
+  if (req.params.id !== req.body.id) {
+    const message = `Request path id (${req.params.id}) and request body id (${req.body.id}) must match`;
+    console.error(message);
+    return res.status(400).send(message);
+  }
+  console.log(`Updating shopping list item \`${req.params.id}\``);
+  ShoppingList.update({
+    id: req.params.id,
+    name: req.body.name,
+    budget: req.body.budget
+  });
+  res.status(204).end();
+});
+
+app.put('/recipes/:id', jsonParser, (req, res) => {
+  const requiredFields = ['name', 'ingredients', 'id'];
+  for (let i=0; i<requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+
+  if (req.params.id !== req.body.id) {
+    const message = `Request path id (${req.params.id}) and request body id (${req.body.id}) must match`;
+    console.error(message);
+    return res.status(400).send(message);
+  }
+  console.log(`Updating recipes item \`${req.params.id}\``);
+  Recipes.update({
+    id: req.params.id,
+    name: req.body.name,
+    ingredients: req.body.ingredients
+  });
+  res.status(204).end();
+});
+
+// when DELETE request comes in with an id in path,
+// try to delete that item from ShoppingList.
+app.delete('/shopping-list/:id', (req, res) => {
+  ShoppingList.delete(req.params.id);
+  console.log(`Deleted shopping list item \`${req.params.ID}\``);
+  res.status(204).end();
+});
+
+
+app.get('/recipes', (req, res) => {
+  res.json(Recipes.get());
+});
+
+app.post('/recipes', jsonParser, (req, res) => {
+  // ensure `name` and `budget` are in request body
+  const requiredFields = ['name', 'ingredients'];
+  for (let i=0; i<requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+  const item = Recipes.create(req.body.name, req.body.ingredients);
+  res.status(201).json(item);
+});
+
+app.delete('/recipes/:id', (req, res) => {
+  Recipes.delete(req.params.id);
+  console.log(`Deleted recipe \`${req.params.ID}\``);
+  res.status(204).end();
 });
 
 app.listen(process.env.PORT || 8080, () => {
